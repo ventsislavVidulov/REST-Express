8fab61c1718df31efc0ac48c723249faaf0b146d
xcen3k <xcen3k@gmail.com>
2023-03-27 19:45:18 +0300

    add resource service

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
+import mongoose from 'mongoose';
 import express from 'express';
 
-// const express = require('express');
+import cors from './src/middlewares/cors.js';
+import resourceController from './src/controllers/resource.js'
 
-const app = express();
+async function start() {
+    try {
+        const db = await mongoose.connect('mongodb://127.0.0.1:27017/rest');
+        console.log('DB ready')
+    } catch (error) {
+        console.log('Erorr connectiong to DB');;
+        return process.exit(1);
+    }
+    const app = express();
 
-app.get('/data/catalog', (req, res) => {
-    res.json([])
-});
 
-app.listen(3030, () => console.log('REST service started on port 3030'));
+    app.use(express.json());
+    app.use(cors());
+
+    app.use('/data/catalog', resourceController)
+
+    app.listen(3030, () => console.log('REST service started on port 3030'));
+
+}
+
+start();

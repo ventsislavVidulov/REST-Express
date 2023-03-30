ce5120b8cc4b50ad454dc28d823b5505e34a8930
xcen3k <xcen3k@gmail.com>
2023-03-31 00:04:37 +0300

    Update auth.js

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,6 +1,22 @@
+import api from '../services/user.js'
+
 export function authMiddleware() {
     return function (req, res, next) {
-        console.log(req.headers['x-authorization']);
+        const token = req.headers['x-authorization']
+
+        if (token) {
+            try {
+                const payload = api.validateToken(token);
+            
+                req.user = {
+                    email: payload.email,
+                    _id: payload._id,
+                    token
+                }
+            } catch (error) {
+                return res.status(401).json({ message: 'Invalid access token' })
+            }
+        }
 
         next();
     }

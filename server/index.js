8232eaf3cc52e6f39c393bfbfe04b3c26122c1c2
xcen3k <xcen3k@gmail.com>
2023-03-31 21:29:33 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ async function start() {
     }
     const app = express();
 
-
+    app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
     app.use(cors());
     app.use(authMiddleware());
